4a6241652e16eda952b2966e622201ed6c0279e0
Pranav S Kurup <pranavs.kurup@cod3byte.com>
2018-05-15 18:20:04 +0545

    Created class CrealyticsAppRunner.java with main method. pom.xml updated with spring boot maven plugin and spring boot version, CrealyticsAppRunner.java specified as main class

---
+++ src/main/java/quest/crealytics/CrealyticsAppRunner.java
@@ -0,0 +1,10 @@
+package quest.crealytics;
+
+/**
+ * Created by Pranav S Kurup on 5/15/2018.
+ */
+public class CrealyticsAppRunner {
+    public static void main(String[] args) {
+        
+    }
+}
