0a8237c93f6618cf88dfabb348a4078a370f8e66
Pranav S Kurup <pranavs.kurup@cod3byte.com>
2018-05-15 20:09:19 +0545

    ReportHeader added to parse csv header ReportSite updated to retrieve Site details

--- src/main/java/quest/crealytics/model/ReportSite.java
+++ src/main/java/quest/crealytics/model/ReportSite.java
@@ -1,7 +1,41 @@
 package quest.crealytics.model;
 
+import quest.crealytics.exception.SiteNotFoundException;
+
 /**
  * Created by Pranav S Kurup on 5/15/2018.
  */
 public enum ReportSite {
+
+    DESKTOP_WEB("desktop_web"), MOBILE_WEB("mobile_web"), ANDROID("android"), IOS("iOS");
+
+    private String siteType;
+
+    ReportSite(String siteType) {
+        this.siteType = siteType;
+    }
+
+    public static ReportSite ofSite(String site) {
+        ReportSite currentSite = null;
+        ReportSite[] values = ReportSite.values();
+        for (int i = 0; i < values.length; i++) {
+            ReportSite siteEnum = values[i];
+            if (siteEnum.getSiteType().equalsIgnoreCase(site.replace(' ', '_'))) {
+                currentSite = siteEnum;
+            }
+        }
+        if (currentSite == null) {
+            throw new SiteNotFoundException("Invalid value for Site: " + site);
+        }
+        return currentSite;
+    }
+
+    public String getSiteType() {
+        return siteType;
+    }
+
+    public String getSiteTypeViewName() {
+        return siteType.replace('_', ' ');
+    }
+
 }
