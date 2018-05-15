0e0d0980d2dc774d98ad951280b94e2992905638
Pranav S Kurup <pranavs.kurup@cod3byte.com>
2018-05-15 18:22:58 +0545

    Updated pom.xml with spring boot dependencies. Updated CrealyticsAppRunner.java with spring runner

--- src/main/java/quest/crealytics/CrealyticsAppRunner.java
+++ src/main/java/quest/crealytics/CrealyticsAppRunner.java
@@ -1,10 +1,14 @@
 package quest.crealytics;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Pranav S Kurup on 5/15/2018.
  */
+@SpringBootApplication
 public class CrealyticsAppRunner {
     public static void main(String[] args) {
-        
+        SpringApplication.run(CrealyticsAppRunner.class, args);
     }
 }
