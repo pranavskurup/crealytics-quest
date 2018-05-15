2f24f64167a69cb01f3c2390ca076902c2d0bdac
Pranav S Kurup <pranavs.kurup@cod3byte.com>
2018-05-15 20:34:51 +0545

    Populate database on startup

---
+++ src/main/java/quest/crealytics/config/AppConfig.java
@@ -0,0 +1,10 @@
+package quest.crealytics.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Pranav S Kurup on 5/15/2018.
+ */
+@Configuration
+public class AppConfig {
+}
